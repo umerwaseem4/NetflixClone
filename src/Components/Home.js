b2d1ac1c4b0047dde0e612f73db31ea559da613d
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-08-06 14:20:13 +0500

    added footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -41,6 +41,34 @@ const Home = () => {
         video="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v"
       />
       <Accordion />
+      <div className="Footer">
+        <footer class="site-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p class="text-justify">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
+                  error magni a maiores quisquam quas sed odio veritatis saepe
+                  dolore molestias perspiciatis facere reprehenderit at, enim
+                  doloremque, nemo rem minus?
+                </p>
+              </div>
+            </div>
+            <hr />
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">
+                  Copyright &copy; 2020 All Rights Reserved by
+                  <a href="#"> Umer Waseem</a>.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
